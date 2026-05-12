23de7a71bc51cd8231bd4b386eb8fb116ef405c5
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-05-12 21:48:51 +0100

    feat: contact message models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,7 @@ from app.models.refresh_token import RefreshToken
 from app.models.agent import Agent
 from app.models.agent_skill import AgentSkill
 from app.models.skill import Skill
+from app.models.contact import ContactMessage
 
 __all__ = [
     "Base",
@@ -13,4 +14,5 @@ __all__ = [
     "Skill",
     "AgentSkill",
     "RefreshToken",
+    "ContactMessage",
 ]
